b5fbef7862a9d80bed2bb0dee17254a38e64c11b
CELSO FERNANDO REYES MENCIA <cfreyesm@unah.hn>
2021-02-08 21:32:44 -0600

    Agregamos una lista de productos

---
+++ QuieroPizza/QuieroPizza.web/Controllers/ProductosController.cs
@@ -0,0 +1,40 @@
+﻿using QuieroPizza.web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuieroPizza.web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto1 = new ProductoModel();
+            producto1.Id = 1;
+            producto1.Descripcion = "Pizza 6 Quesos";
+
+            var producto2 = new ProductoModel();
+            producto2.Id = 2;
+            producto2.Descripcion = "Pizza 4 Estaciones";
+
+            var producto3 = new ProductoModel();
+            producto3.Id = 3;
+            producto3.Descripcion = "Pizza Jamon y Queso";
+
+            var producto4 = new ProductoModel();
+            producto4.Id = 4;
+            producto4.Descripcion = "Pizza Peperoni";
+
+            var ListaProductos = new List<ProductoModel>();
+            ListaProductos.Add(producto1);
+            ListaProductos.Add(producto2);
+            ListaProductos.Add(producto3);
+            ListaProductos.Add(producto4);
+
+            return View(ListaProductos);
+        }
+    }
+}
